91f4b3a9b15136170d729326bb6b9fa31e15baf1
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2020-07-08 17:54:42 -0400

    tags

--- tags.py
+++ tags.py
@@ -2,8 +2,27 @@
 
 import requests
 import html
+import json
+import os
+
+TAG_DATA_FILE = os.path.split(os.path.abspath(__file__))[0] + '/tags.json'
+
+category_to_tag = {}
+tag_to_category = {}
+
+def flip_tags(dct, key=''):
+    '''generate tag_to_category from category_to_tag
+    called by scrape_tags() and load_tags(), do not call directly'''
+    global tag_to_category
+    for k in dct:
+        if isinstance(dct[k], str):
+            tag_to_category[k] = key
+        else:
+            flip_tags(dct[k], k)
 
 def scrape_tags():
+    '''scrape tag database from Apertium wiki'''
+    global category_to_tag
     current = []
     all_tags = {}
     r = requests.get('http://wiki.apertium.org/w/index.php?title=List_of_symbols&action=raw')
@@ -35,13 +54,26 @@ def scrape_tags():
                 all_tags[current[0]][current[1]][tag] = gloss
             elif len(current) == 3:
                 all_tags[current[0]][current[1]][current[2]][tag] = gloss
-    return all_tags
+    with open(TAG_DATA_FILE, 'w') as f:
+        json.dump(all_tags, f)
+    category_to_tag = all_tags
+    flip_tags(all_tags)
+
+def load_tags():
+    '''load tag database from file'''
+    global category_to_tag
+    with open(TAG_DATA_FILE) as f:
+        try:
+            category_to_tag = json.load(f)
+        except:
+            raise Exception("Tag data file missing or invalid. Please (re)run %s and try again." % os.path.abspath(__file__))
+    flip_tags(category_to_tag)
+
+def lookup_tag(tag):
+    global tag_to_category
+    if len(tag_to_category) == 0:
+        load_tags()
+    return tag_to_category.get(tag)
 
 if __name__ == '__main__':
-    import sys, json
-    if len(sys.argv) != 2:
-        print('Usage: %s output_file' % sys.argv[0])
-    f = open(sys.argv[1], 'w')
-    blob = scrape_tags()
-    json.dump(blob, f)
-    f.close()
+    scrape_tags()
